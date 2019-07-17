6929fd18b88cc68b30eb657f53e494a854443b77
Emil John Lopez <emiljohn24@gmail.com>
2019-07-17 08:24:19 +0800

    More tests

--- src/test.c
+++ src/test.c
@@ -1,13 +1,14 @@
 #include "deck.h"
 #include "clean.h"
 int main(){
+    srand(time(NULL));
     for (int i = 0; i != 1000; i++){
         deck_t *deck = generate_std_deck();
         hand_t *my_hand = get_hand(5, deck);
         hand_t *enemy_hand = get_hand(5, deck);
         shuffle_deck(deck, 100);
-        sort_hand((hand_t*)deck);
-        //printf("Deck:\n%s\n\n\n", get_deck_text(deck));
+        //sort_hand((hand_t*)deck);
+        printf("Deck:\n%s\n\n\n", get_deck_text(deck));
         sort_hand(my_hand);
         sort_hand(enemy_hand);
         printf("My hand: \n%s\n", get_hand_text(my_hand));
@@ -16,7 +17,7 @@ int main(){
         clean_hand(my_hand);
         clean_hand(enemy_hand);
         clean_deck(deck);
-    }
     //
+    }
     return 0;
 }
